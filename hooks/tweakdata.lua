af4ea0cc79458d4ea1da48c9c2a88ed5a727fd50
VxWolf <vxwolf368@gmail.com>
2020-08-15 17:11:13 +1000

    Stuff
    General
    -Fade changes
    Day 1
    -Added some more paths for AI
    -Changed the trigger for approching the forklift-proof wall

--- hooks/tweakdata.lua
+++ hooks/tweakdata.lua
@@ -1,15 +1,15 @@
-	tweak_data.overlay_effects.fade_out_mex = {
+	tweak_data.overlay_effects.fade_out_boworks = {
 			text_to_upper = true,
-			sustain = 4,
+			sustain = 0.5,
 			localize = true,
-			fade_in = 1,
+			fade_in = 0.25,
 			text_blend_mode = "add",
-			fade_out = 1,
+			fade_out = 0.25,
 			font = "fonts/font_large_mf",
 			text = "heist_physics_transition",
-			font_size = 44,
+			font_size = 88,
 			blend_mode = "normal",
-			color = Color(1, 0, 0, 0),
+			color = Color(1, 0.1, 0.1, 0.1),
 			timer = TimerManager:main(),
 			text_color = Color(255, 77, 198, 255) / 255
 		}
