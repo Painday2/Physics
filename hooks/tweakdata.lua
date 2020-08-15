f8555a5bf9bda3ceb21978a0e6091e345bec29b4
Cupcake <meowcats2121@gmail.com>
2020-08-14 22:08:04 -0600

    fixed transition

--- hooks/tweakdata.lua
+++ hooks/tweakdata.lua
@@ -1,23 +1,15 @@
-Hooks:PostHook(TweakData, "init", "init_boworks", function(self, tweak_data)
-	
-	self.overlay_effects.fade_out_test = {
-		text_to_upper = true,
-		sustain = 30,
-		blend_mode = "normal",
-		fade_in = 3,
-		text_blend_mode = "add",
-		fade_out = 0,
-		font = "fonts/font_large_mf",
-		text = [[
-Great job, gang!
-You've reached the end of our E3 demo.
-Play the full version soon to get your full PAYDAY!]],
-		font_size = 44,
-		play_paused = true,
-		color = Color(1, 0, 0, 0),
-		timer = TimerManager:main(),
-		text_color = Color(255, 255, 204, 0) / 255
-	}
-	
-	
-end)
+	tweak_data.overlay_effects.fade_out_mex = {
+			text_to_upper = true,
+			sustain = 4,
+			localize = true,
+			fade_in = 1,
+			text_blend_mode = "add",
+			fade_out = 1,
+			font = "fonts/font_large_mf",
+			text = "heist_physics_transition",
+			font_size = 44,
+			blend_mode = "normal",
+			color = Color(1, 0, 0, 0),
+			timer = TimerManager:main(),
+			text_color = Color(255, 77, 198, 255) / 255
+		}
