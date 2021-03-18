5a935422f526410ddd379c21466c5dde14075330
VxWolf <vxwolf368@gmail.com>
2021-03-19 00:08:56 +1100

    You're out of touch, I'm out of time
    -Added contractor info so heist/broker no longer crashes
    Day 1
    -Added more cover to various locations
    Day 2
    -Adjusted Spawns on the outside streets
    -Fixed two computers spawning in one of the streetblocker security rooms

---
+++ hooks/dialogmanager.lua
@@ -0,0 +1,9 @@
+Hooks:Add("LocalizationManagerPostInit", "SpainLocalization", function(loc)
+	if Global.game_settings.level_id == "physics_citystreets" or Global.game_settings.level_id == "physics_tower" or Global.game_settings.level_id == "physics_core"  then
+		LocalizationManager:add_localized_strings({
+		["menu_description_bain"] = "Cocke's Plan"
+		})
+	else
+		return
+	end
+end)
