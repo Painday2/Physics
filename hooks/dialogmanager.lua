ff305d990927f1257f7218e37258b795e59b1d9a
VxWolf <vxwolf368@gmail.com>
2021-03-20 23:27:07 +1100

    Pauline Pantsdown - I don't like it
    Day 1
    -Hopefully fixed securing multiple bags of guilt not progressing the heist
    -Added XP
    Day 2
    -Fixes to streetblocker objective
    -Fixed skydive jump making every player skydive
    -Added XP
    Day 3
    -Added XP

--- hooks/dialogmanager.lua
+++ hooks/dialogmanager.lua
@@ -1,7 +1,7 @@
 Hooks:Add("LocalizationManagerPostInit", "SpainLocalization", function(loc)
 	if Global.game_settings.level_id == "physics_citystreets" or Global.game_settings.level_id == "physics_tower" or Global.game_settings.level_id == "physics_core"  then
 		LocalizationManager:add_localized_strings({
-		["menu_description_bain"] = "Cocke's Plan"
+		["menu_description_locke"] = "Cocke's Plan"
 		})
 	else
 		return
