b4df8ee42fc05eae08a8f95a89f0e792b6501e04
Cupcake <meowcats2121@gmail.com>
2021-03-27 20:26:42 -0600

    I still work on this thing?
    possibly fixed contours

--- hooks/copbase.lua
+++ hooks/copbase.lua
@@ -3,25 +3,11 @@ local material_config_paths = {
   "units/mainman/characters/ene_bo/ene_bo",
   "units/matthelzor/characters/ford/ford",
   "units/matthelzor/characters/ford_civ/ford_civ",
-  "units/pd2_mod_bofa/characters/sbz_units/ene_sbz_mp5/ene_sbz_mp5",
-  "units/pd2_mod_bofa/characters/sbz_units/ene_sbz_r870/ene_sbz_r870",
-  "units/pd2_mod_bofa/characters/sbz_units/ene_sbz_heavy_m4/ene_sbz_heavy_m4",
-  "units/pd2_mod_bofa/characters/sbz_units/ene_sbz_heavy_r870/ene_sbz_heavy_r870",
-  "units/pd2_mod_bofa/characters/sbz_units/ene_sbz_shield_c45/ene_sbz_shield_c45",
-  "units/pd2_mod_bofa/characters/sbz_units/ene_sbz_shield_mp9/ene_sbz_shield_mp9",
-  "units/pd2_mod_bofa/characters/ovk_units/ene_ovk_m4/ene_ovk_m4",
-  "units/pd2_mod_bofa/characters/ovk_units/ene_ovk_r870/ene_ovk_r870",
-  "units/pd2_mod_bofa/characters/ovk_units/ene_ovk_heavy_r870/ene_ovk_heavy_r870",
-  "units/pd2_mod_bofa/characters/ovk_units/ene_ovk_heavy_m4/ene_ovk_heavy_m4",
-  "units/pd2_mod_bofa/characters/ovk_units/ene_ovk_shield_c45/ene_ovk_shield_c45",
-  "units/pd2_mod_bofa/characters/ovk_units/ene_ovk_shield_mp9/ene_ovk_shield_mp9",
-  "units/pd2_mod_bofa/characters/bofa_units/ene_bofa_ump/ene_bofa_ump",
-  "units/pd2_mod_bofa/characters/bofa_units/ene_bofa_r870/ene_bofa_r870",
-  "units/pd2_mod_bofa/characters/bofa_units/ene_bofa_g36/ene_bofa_g36",
-  "units/pd2_mod_bofa/characters/bofa_units/ene_bofa_heavy_g36/ene_bofa_heavy_g36",
-  "units/pd2_mod_bofa/characters/bofa_units/ene_bofa_heavy_r870/ene_bofa_heavy_r870",
-  "units/pd2_mod_bofa/characters/bofa_units/ene_bofa_shield_c45/ene_bofa_shield_c45",
-  "units/pd2_mod_bofa/characters/bofa_units/ene_bofa_shield_mp9/ene_bofa_shield_mp9"
+  "units/pd2_mod_bofa/characters/shared_materials/ene_bofa",
+  "units/pd2_mod_bofa/characters/shared_materials/ene_bofa_heavy",
+  "units/pd2_mod_bofa/characters/shared_materials/ene_ovk",
+  "units/pd2_mod_bofa/characters/shared_materials/ene_ovk_heavy",
+  "units/pd2_mod_bofa/characters/shared_materials/ene_sbz"
 }
 
 
