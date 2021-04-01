405594c67ff72a13bbf557b81e5760e42086c1f7
Cupcake <meowcats2121@gmail.com>
2021-03-31 21:49:21 -0600

    removed debug, possiblyf ixed invisible enemies

--- hooks/MissionManager.lua
+++ hooks/MissionManager.lua
@@ -1,20 +1,3 @@
 function MissionManager:set_fading_debug_enabled(enabled)
 	self._fading_debug_output:show()
 end
-
-function MissionManager:add_fading_debug_output(debug, color, as_subtitle)
-	if as_subtitle then
-		self:_show_debug_subtitle(debug, color)
-	else
-		local stuff = {
-			" -",
-			" \\",
-			" |",
-			" /"
-		}
-		self._fade_index = (self._fade_index or 0) + 1
-		self._fade_index = self._fade_index > #stuff and self._fade_index and 1 or self._fade_index
-
-		self._fading_debug_output:script().log(stuff[self._fade_index] .. " " .. debug, color, nil)
-	end
-end
